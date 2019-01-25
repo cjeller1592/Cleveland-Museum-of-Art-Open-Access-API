7f9c0cdf8386c931ef21f0d70848cfdd3b2deb45
CJ Eller <45696734+cjeller1592@users.noreply.github.com>
2019-01-25 10:25:46 -0500

    Make openAccess class

--- cma.py
+++ cma.py
@@ -1,32 +1,37 @@
 import requests
 import json
 
+class openAccess(object):
+    
+    def search(self, q, limit):
 
-def search(q, limit):
-
-    try:
-        data = {'q': q,
+        try:
+            data = {'q': q,
                 'limit': limit
                 }
 
-        r = requests.get('https://openaccess-api.clevelandart.org/api/artworks/',
+            r = requests.get('https://openaccess-api.clevelandart.org/api/artworks/',
                     headers={'Content-Type':'application/json'}, params=data)
 
-    except Exception as e:
-        print('Exception raised on search: %s' % e)
-        return e
+        except Exception as e:
+            print('Exception raised on search: %s' % e)
+            return e
 
-    return r.json()
+        result = r.json()['data']
+        
+        return result
 
-def find(id):
-    try:
-        r = requests.get('https://openaccess-api.clevelandart.org/api/artworks/' + id,
+    
+    def find(self, id):
+
+        try:
+            r = requests.get('https://openaccess-api.clevelandart.org/api/artworks/' + id,
                     headers={'Content-Type':'application/json'})
 
-    except Exception as e:
-        print('Exception raised on find: %s' % e)
-        return e
+        except Exception as e:
+            print('Exception raised on find: %s' % e)
+            return e
 
-    work = r.json()['data']
+        result = r.json()['data']
 
-    return work
+        return result
